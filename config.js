1ef7fe31b34f2cb6cc2c932880f40d4baf2d734d
wisnu23055 <wisnu.23055@mhs.unesa.ac.id>
2025-10-14 10:27:05 +0700

    edited

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Ganti value dari Supabase dan OMDB API
+// Masukkan Value dari Supabase dan OMDB API
 window.CONFIG = {
   SUPABASE_URL: "https://ecxlgswhpyscwsyyhoxn.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjeGxnc3docHlzY3dzeXlob3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3MTQyNzAsImV4cCI6MjA3NTI5MDI3MH0.R918rH2I6Ayp1c3L7FS7zrhQaShJpP1sWTOtTqpROFI",
